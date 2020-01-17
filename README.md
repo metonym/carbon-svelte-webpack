d120d991af925d9e63b462d7e90436f2f74f4064
Eric Liu <ericyl.us@gmail.com>
2020-01-17 08:25:34 -0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# carbon-svelte-webpack
+
+> Svelte + Webpack template for building apps with the [Carbon Design System](https://www.carbondesignsystem.com/)
+
+This template demonstrates a viable set-up for consuming [Carbon Components Svelte](https://github.com/IBM/carbon-components-svelte) in Svelte.
+
+- [Carbon Components Svelte](https://github.com/IBM/carbon-components-svelte) (components)
+- [Carbon Icons Svelte](https://github.com/IBM/carbon-icons-svelte) (iconography)
+- [Carbon Components](https://github.com/carbon-design-system/carbon/tree/master/packages/components) (styling)
+
+## Getting Started
+
+Clone the repository and install its dependencies.
+
+```bash
+git clone git@github.com:metonym/carbon-svelte-webpack.git
+cd carbon-svelte-webpack
+yarn install
+```
+
+## Available Scripts
+
+### `yarn develop`
+
+Runs the app in development mode. Visit [http://localhost:8080](http://localhost:8080) to view the app.
+
+Customize the port number using the `port` flag:
+
+```bash
+yarn start --port 3000
+```
+
+### `yarn build`
+
+Builds the app for production. Compiled assets are outputted to `build/`.
+
+## License
+
+[MIT](LICENSE)
---
+++ src/App.svelte
@@ -0,0 +1,104 @@
+<script>
+  import {
+    Button,
+    FormGroup,
+    RadioButtonGroup,
+    RadioButton,
+    Select,
+    SelectItem
+  } from "carbon-components-svelte";
+  import Add16 from "carbon-icons-svelte/lib/Add16";
+
+  if (process.env.NODE_ENV === "development") {
+    console.group("carbon-svelte-webpack");
+    console.info("This message only appears in development.");
+    console.groupEnd();
+  }
+
+  let size = "default";
+  let toggle = false;
+
+  let selectedOption = "two";
+</script>
+
+<style>
+  header {
+    margin-top: 2.5rem;
+  }
+
+  .grid-lines * {
+    outline: 1px solid rgba(0, 0, 0, 0.15);
+  }
+
+  .bx--row {
+    margin-bottom: 4rem;
+  }
+
+  .col--section {
+    margin-bottom: 2.5rem;
+  }
+</style>
+
+<div class="bx--grid" class:grid-lines={toggle}>
+  <header class="bx--row">
+    <div class="bx--col">
+      <h1>Carbon Components Svelte</h1>
+      <div style="margin-top: 2.5rem">
+        <Button
+          kind="tertiary"
+          size="small"
+          on:click={() => {
+            toggle = !toggle;
+          }}>
+          {toggle ? 'Hide' : 'Show'} grid lines
+        </Button>
+      </div>
+    </div>
+  </header>
+  <div class="bx--row">
+    <div class="bx--col-lg-2">
+      <h4>Typography</h4>
+    </div>
+    <div class="bx--col">
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>Paragraph</p>
+    </div>
+  </div>
+  <div class="bx--row">
+    <div class="bx--col-lg-2">
+      <h4>Components</h4>
+    </div>
+    <div class="bx--col">
+      <div class="col--section">
+        <Button {size}>Primary button</Button>
+        <Button {size} kind="secondary">Secondary button</Button>
+        <Button {size} kind="tertiary">Tertiary button</Button>
+        <Button {size} kind="ghost">Ghost button</Button>
+        <Button {size} kind="danger">Danger button</Button>
+      </div>
+      <div class="col--section">
+        <FormGroup legendText="Button Size (size)">
+          <RadioButtonGroup
+            orientation="vertical"
+            legend="Button Size (size)"
+            bind:selected={size}>
+            <RadioButton value="default" id="default" labelText="default" />
+            <RadioButton value="field" id="field" labelText="field" />
+            <RadioButton value="small" id="small" labelText="small" />
+          </RadioButtonGroup>
+        </FormGroup>
+      </div>
+      <Select bind:selected={selectedOption}>
+        <SelectItem value="one">Option 1</SelectItem>
+        <SelectItem value="two">Option 2</SelectItem>
+        <SelectItem value="three">Option 3</SelectItem>
+      </Select>
+      <div style="margin-top: 1rem;">Selected option: {selectedOption}</div>
+    </div>
+  </div>
+</div>
